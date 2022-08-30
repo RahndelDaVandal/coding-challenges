ec18eec480647ede7b8db16e46300cfb21c07764
Connor Sahleen <Connor.Sahleen@gmail.com>
2022-08-30 15:40:39 -0500

    updated name in Cargo.toml to confirm to snake_case

---
+++ Google-08-29-22/target/debug/deps/_08_29_22-1c8722be3149d562.d
@@ -0,0 +1,5 @@
+/home/pi/wip/coding-challenges/_08-29-22/target/debug/deps/_08_29_22-1c8722be3149d562: src/main.rs
+
+/home/pi/wip/coding-challenges/_08-29-22/target/debug/deps/_08_29_22-1c8722be3149d562.d: src/main.rs
+
+src/main.rs:
---
+++ Google-08-29-22/target/debug/deps/google_08_29_22-bf382d8752a62a36.d
@@ -0,0 +1,5 @@
+/home/pi/wip/coding-challenges/Google-08-29-22/target/debug/deps/google_08_29_22-bf382d8752a62a36: src/main.rs
+
+/home/pi/wip/coding-challenges/Google-08-29-22/target/debug/deps/google_08_29_22-bf382d8752a62a36.d: src/main.rs
+
+src/main.rs:
