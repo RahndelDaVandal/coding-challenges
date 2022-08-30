43340c9f926695736255dc385181f8a99941740e
Connor Sahleen <Connor.Sahleen@gmail.com>
2022-08-30 01:57:08 -0500

    solved

---
+++ _08-29-22/target/debug/_08-29-22.d
@@ -0,0 +1,1 @@
+/home/pi/wip/coding-challenges/_08-29-22/target/debug/_08-29-22: /home/pi/wip/coding-challenges/_08-29-22/src/main.rs
---
+++ _08-29-22/target/debug/deps/_08_29_22-d8c25c1ebac38812.d
@@ -0,0 +1,5 @@
+/home/pi/wip/coding-challenges/_08-29-22/target/debug/deps/_08_29_22-d8c25c1ebac38812: src/main.rs
+
+/home/pi/wip/coding-challenges/_08-29-22/target/debug/deps/_08_29_22-d8c25c1ebac38812.d: src/main.rs
+
+src/main.rs:
